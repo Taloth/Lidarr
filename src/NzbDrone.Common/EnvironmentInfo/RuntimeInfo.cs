ead53cdb28bb35521873a76aa765661f4270e314
ta264 <ta264@users.noreply.github.com>
2019-09-01 22:30:52 +0100

    Generally convert MonoOnly to PosixOnly

--- src/NzbDrone.Common/EnvironmentInfo/RuntimeInfo.cs
+++ src/NzbDrone.Common/EnvironmentInfo/RuntimeInfo.cs
@@ -156,6 +156,7 @@ namespace NzbDrone.Common.EnvironmentInfo
             {
                 var currentAssemblyLocation = typeof(RuntimeInfo).Assembly.Location;
                 if (currentAssemblyLocation.ToLower().Contains("_output")) return true;
+                if (currentAssemblyLocation.ToLower().Contains("_tests")) return true;
             }
             catch
             {
@@ -166,6 +167,7 @@ namespace NzbDrone.Common.EnvironmentInfo
             if (lowerCurrentDir.Contains("vsts")) return true;
             if (lowerCurrentDir.Contains("buildagent")) return true;
             if (lowerCurrentDir.Contains("_output")) return true;
+            if (lowerCurrentDir.Contains("_tests")) return true;
 
             return false;
         }
