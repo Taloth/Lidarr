90f5bac6b76146ba477831a0102bd2a4e625b5bd
ta264 <ta264@users.noreply.github.com>
2019-09-01 21:53:43 +0100

    Remove growl and prowl

--- src/NzbDrone.Common/EnvironmentInfo/RuntimeInfo.cs
+++ src/NzbDrone.Common/EnvironmentInfo/RuntimeInfo.cs
@@ -65,6 +65,11 @@ namespace NzbDrone.Common.EnvironmentInfo
         {
             get
             {
+                if (OsInfo.IsNotWindows)
+                {
+                    return false;
+                }
+
                 try
                 {
                     var principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
